e86b871476dc49c5ff31a2d20c237fc650e260e2
Moises Sacal <moisbo@gmail.com>
2021-12-24 10:04:23 +1100

    switched to exports instead of module.export

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,17 +1,16 @@
-module.exports = {
-  loadConfiguration: require("./configuration").loadConfiguration,
-  filterPrivateInformation: require("./configuration").filterPrivateInformation,
-  getLogger: require('./logger').getLogger,
-  routeUser: require('../middleware/auth').routeUser,
-  generateToken: require('./jwt').generateToken,
-  verifyToken: require('./jwt').verifyToken,
-  setupBeforeAll: require('./test-utils').setupBeforeAll,
-  setupBeforeEach: require('./test-utils').setupBeforeEach,
-  teardownAfterAll: require('./test-utils').teardownAfterAll,
-  teardownAfterEach: require('./test-utils').teardownAfterEach,
-  testOCFLConf: require('./test-utils').testOCFLConf,
-  testHost: require('./test-utils').testHost,
-  testLicense: require('./test-utils').testLicense,
-  testIdentifier: require('./test-utils').testIdentifier,
-  testCreate: require('./test-utils').testCreate
-}
+export { loadConfiguration, filterPrivateInformation } from "./configuration";
+export { getLogger } from './logger';
+export { routeUser } from '../middleware/auth';
+export { generateToken, verifyToken } from './jwt';
+export {
+  setupBeforeAll,
+  setupBeforeEach,
+  teardownAfterAll,
+  teardownAfterEach,
+  testOCFLConf,
+  testHost,
+  testLicense,
+  testIdentifier,
+  testCreate,
+} from './test-utils';
+
