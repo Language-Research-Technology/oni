33112b3d10abe044132fce024691f56fb6fab264
Moises Sacal <moisbo@gmail.com>
2022-04-04 14:52:59 +1000

    invert link

--- ui/src/components/RequestDialog.component.vue
+++ ui/src/components/RequestDialog.component.vue
@@ -1,16 +1,18 @@
 <template>
   <el-dialog v-model="dialogVisible" v-on:close="this.closeDialog()" title="Request Access" width="40%" draggable>
     <el-row :gutter="10">
-      <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12" >
-      Contact: <br/><a class="underline" :href="'mailto: '+ emailHelp">{{emailHelp}}</a>
+      <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+        Contact: <br/><a class="underline" :href="'mailto: '+ emailHelp">{{ emailHelp }}</a>
       </el-col>
       <el-col :xs="4" :sm="4" :md="4" :lg="4" :xl="4">
-        <el-divider direction="vertical" />
+        <el-divider direction="vertical"/>
       </el-col>
-      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8" >
-        <el-button>
-          <el-link href="/login">Login</el-link>
-        </el-button>
+      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
+        <el-link href="/login">
+          <el-button>
+            Login
+          </el-button>
+        </el-link>
       </el-col>
     </el-row>
     <template #footer>
