81c36a258c28e1c19785b202d7a02d47726022be
Moises Sacal <moisbo@gmail.com>
2022-06-09 15:21:51 +1000

    uncomment production build

--- build-production-containers.sh
+++ build-production-containers.sh
@@ -15,48 +15,48 @@ exit 0
 
 read -p '>> Build the code? [y|N] ' resp
 if [ "$resp" == "y" ] ; then
-    # echo '>> Building the API code'
-    # docker build --rm -t arkisto/oni-api:latest -f Dockerfile.api-build .
-    # echo
+     echo '>> Building the API code'
+     docker build --rm -t arkisto/oni-api:latest -f Dockerfile.api-build .
+     echo
 
-    # echo '>> Building the UI code'
-    # cd ui
-    # # npm run build
-    # docker run -it --rm \
-    #     -v $PWD:/srv/ui \
-    #     -w /srv/ui node:16 bash -l -c "npm run build"
-    # cd -
-    # echo
+     echo '>> Building the UI code'
+     cd ui
+     # npm run build
+     docker run -it --rm \
+         -v $PWD:/srv/ui \
+         -w /srv/ui node:16 bash -l -c "npm run build"
+     cd -
+     echo
 fi
 
 read -p '>> Build the containers? [y|N] ' resp
 if [ "$resp" == "y" ] ; then
-    # cd api
-    # npm version --no-git-tag-version ${VERSION}
-    # cd ../ui
-    # npm version --no-git-tag-version ${VERSION}
-    # cd ..
-    # git tag v${VERSION}
-    # git commit -a -m "tag and bump version"
+     cd api
+     npm version --no-git-tag-version ${VERSION}
+     cd ../ui
+     npm version --no-git-tag-version ${VERSION}
+     cd ..
+     git tag v${VERSION}
+     git commit -a -m "tag and bump version"
 
-    # echo "Building API container"
-    # docker tag arkisto/oni-api:latest arkisto/oni-api:${VERSION}
-    # # docker rmi $(docker images | grep none | awk '{print $3}')
+     echo "Building API container"
+     docker tag arkisto/oni-api:latest arkisto/oni-api:${VERSION}
+     # docker rmi $(docker images | grep none | awk '{print $3}')
 
-    # echo "Building UI container"
-    # docker build --rm -t arkisto/oni-ui:latest -f Dockerfile.ui-build .
-    # docker tag arkisto/oni-ui:latest arkisto/oni-ui:${VERSION}
+     echo "Building UI container"
+     docker build --rm -t arkisto/oni-ui:latest -f Dockerfile.ui-build .
+     docker tag arkisto/oni-ui:latest arkisto/oni-ui:${VERSION}
 
-    # # docker rmi $(docker images | grep none | awk '{print $3}')
-    # echo
+     # docker rmi $(docker images | grep none | awk '{print $3}')
+     echo
 fi
 
 read -p '>> Push the containers to docker hub? [y|N] ' resp
 if [ "$resp" == "y" ] ; then
-    # echo "Pushing built containers to docker hub"
-    # docker login
-    # docker push arkisto/oni-api:latest
-    # docker push arkisto/oni-api:${VERSION}
-    # docker push arkisto/oni-ui:latest
-    # docker push arkisto/oni-ui:${VERSION}
+#     echo "Pushing built containers to docker hub"
+#     docker login
+#     docker push arkisto/oni-api:latest
+#     docker push arkisto/oni-api:${VERSION}
+#     docker push arkisto/oni-ui:latest
+#     docker push arkisto/oni-ui:${VERSION}
 fi
