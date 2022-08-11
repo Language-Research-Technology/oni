dd90ec25e4b751f48a170ae3d51ffc6f9c6bb7ba
Moises Sacal <moisbo@gmail.com>
2022-08-11 15:37:04 +1000

    adding support for noUris in plain object/meta

--- api/src/routes/object/recordCrate.js
+++ api/src/routes/object/recordCrate.js
@@ -1,39 +1,49 @@
-import { getRecord, getRawCrate, getUridCrate } from '../../controllers/record';
-import { getLogger } from '../../services';
-import { isUndefined } from 'lodash';
+import {getRecord, getRawCrate, getUridCrate} from '../../controllers/record';
+import {getLogger} from '../../services';
+import {isUndefined} from 'lodash';
+import {getCrate} from "../../controllers/recordResolve";
 
 const log = getLogger();
 
-export async function getRecordCrate({ req, res, next, configuration, repository }) {
-  log.debug(`get data ${ req.query.id }`);
-  let record = await getRecord({ crateId: req.query.id });
+export async function getRecordCrate({req, res, next, configuration, repository}) {
+  log.debug(`get data ${req.query.id}`);
+  let getUrid = true;
+  if (!isUndefined(req.query.noUrid)) {
+    getUrid = false;
+  }
+  let record = await getRecord({crateId: req.query.id});
   if (record.data) {
     let crate;
     let version = undefined;
     if (!isUndefined(req.query.version)) {
       version = req.query.version;
-      if(!version){
+      if (!version) {
         res.json({message: 'Please specify a version'}).status(400);
       } else {
-        res.json({ message: 'Version: Not implemented' }).status(400);
+        res.json({message: 'Version: Not implemented'}).status(400);
       }
     } else if (!isUndefined(req.query.raw)) {
       crate = await getRawCrate({repository, crateId: req.query.id});
       res.json(crate);
     } else if (!isUndefined(req.query.zip)) {
-      res.json({ message: 'Zip: Not implemented' }).status(400);
+      res.json({message: 'Zip: Not implemented'}).status(400);
     } else {
-      crate = await getUridCrate({
-        host: configuration.api.host,
-        crateId: req.query.id,
-        typesTransform: configuration.api.rocrate.dataTransform.types,
-        version,
-        repository
-      });
-      res.json(crate);
+      if (getUrid) {
+        crate = await getUridCrate({
+          host: configuration.api.host,
+          crateId: req.query.id,
+          typesTransform: configuration.api.rocrate.dataTransform.types,
+          version,
+          repository
+        });
+        res.json(crate);
+      } else {
+        crate = await getRawCrate({repository, crateId: req.query.id});
+        res.json(crate);
+      }
     }
   } else {
-    res.send({ id: req.query.id, message: 'Not Found' }).status(404);
+    res.send({id: req.query.id, message: 'Not Found'}).status(404);
   }
 }
 
