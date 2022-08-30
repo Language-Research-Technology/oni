5581f545652b4d43f2d2e786efb469940eb8e1d7
Moises Sacal <moisbo@gmail.com>
2022-08-30 18:07:44 +1000

    tag and bump version

--- build-production-containers.sh
+++ build-production-containers.sh
@@ -23,17 +23,17 @@ if [ "$resp" == "y" ] ; then
       -f Dockerfile.api-build .
     echo
 
-    echo '>> Building the UI code'
-    docker buildx build --platform linux/amd64,linux/arm64 \
-      --rm \
-      -t arkisto/oni-ui:latest \
-      -t arkisto/oni-ui:${VERSION} \
-      -f Dockerfile.ui-build .
-    docker buildx build --load \
-      -t arkisto/oni-ui:latest \
-      -t arkisto/oni-ui:${VERSION} \
-      -f Dockerfile.ui-build .
-     echo
+#    echo '>> Building the UI code'
+#    docker buildx build --platform linux/amd64,linux/arm64 \
+#      --rm \
+#      -t arkisto/oni-ui:latest \
+#      -t arkisto/oni-ui:${VERSION} \
+#      -f Dockerfile.ui-build .
+#    docker buildx build --load \
+#      -t arkisto/oni-ui:latest \
+#      -t arkisto/oni-ui:${VERSION} \
+#      -f Dockerfile.ui-build .
+#     echo
 fi
 
 read -p '>> Tag the containers? [y|N] ' resp
@@ -60,12 +60,12 @@ if [ "$resp" == "y" ] ; then
       -t arkisto/oni-api:latest \
       -t arkisto/oni-api:${VERSION} \
       -f Dockerfile.api-build .
-    docker buildx build --platform=linux/amd64,linux/arm64 \
-      --push \
-      --rm \
-      -t arkisto/oni-ui:latest \
-      -t arkisto/oni-ui:${VERSION} \
-      -f Dockerfile.ui-build .
+#    docker buildx build --platform=linux/amd64,linux/arm64 \
+#      --push \
+#      --rm \
+#      -t arkisto/oni-ui:latest \
+#      -t arkisto/oni-ui:${VERSION} \
+#      -f Dockerfile.ui-build .
 
 fi
 
@@ -73,6 +73,6 @@ read -p '>> Remove local container copies? [y|N] ' resp
 if [ "$resp" == "y" ] ; then
     docker rmi arkisto/oni-api:latest
     docker rmi arkisto/oni-api:${VERSION}
-    docker rmi arkisto/oni-ui:latest
-    docker rmi arkisto/oni-ui:${VERSION}
+#    docker rmi arkisto/oni-ui:latest
+#    docker rmi arkisto/oni-ui:${VERSION}
 fi
