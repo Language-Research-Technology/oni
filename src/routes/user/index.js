7f85a960a2d4268350770273692faa2b494cb462
Moises Sacal <moisbo@gmail.com>
2025-04-22 12:52:05 +1000

    working comanage terms

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -6,7 +6,7 @@ import { getUser } from '../../controllers/user.js';
 // import {getCiLogonMemberships} from "../../controllers/cilogon";
 import { encrypt } from '../../services/utils.js';
 import { getUserMemberships } from "../../controllers/userMembership.js";
-import { getTerms, termsAggrement, getPersonId, agreeTerms } from '#src/controllers/comanage.js';
+import { getTerms, termsAggrement, getPersonId, agreeTerms } from '#src/services/comanage.js';
 
 const log = getLogger();
 
@@ -145,40 +145,90 @@ export function setupUserRoutes({ configuration, auth }) {
     }
   });
 
+    /**
+   * @openapi
+   * /user/terms:
+   *   get:
+   *     tags:
+   *       - user
+   *     description: |
+   *                  ### Retrieves User Terms and conditions if not agreed from CoManage
+   *     security:
+   *       - Bearer: []
+   *       - OAuth2:
+   *         - openid
+   *         - profile
+   *         - email
+   *         - org.cilogon.userinfo
+   *         - offline_access
+   *     responses:
+   *       200:
+   *         description: returns terms and conditions if not agreed otherwise false
+   */
   app.get('/terms', async (c) => {
-    try{
-    const user = c.get('user');
-    if (user) {
-      log.debug('checking accepted terms -User: ' + user?.id);
-      console.log(user);
-      const personId = await getPersonId({configuration, user});
-      const agreement = await termsAggrement({ configuration, personId });
-      console.log(agreement);
-      if(!agreement) {
-        const terms = await getTerms({ configuration });
-        return  c.json({terms});
+    try {
+      if(configuration.api.authentication?.identity?.provider === "comanage") {
+        const user = c.get('user');
+        if (user) {
+          log.debug('checking accepted terms -User: ' + user?.id);
+          const personId = await getPersonId({configuration, user});
+          const agreement = await termsAggrement({ configuration, personId });
+          let terms = {};
+          terms = await getTerms({ configuration });
+          if(!agreement) {
+            if(terms.error) {
+              return c.json({error: terms.error});
+            } else {
+              return  c.json(terms);
+            }
+          } else {
+            terms['agreement'] = agreement;
+            return c.json(terms);
+          }
+        } else {
+          return c.notFound();
+        }
       } else {
-        return c.json({terms: false});
+        return c.notFound();
       }
-    } else {
-      return c.notFound();
-    }
-    }catch (e) {
+    } catch (e) {
       return c.json({error: e.message});
     }
   });
 
+  /**
+   * @openapi
+   * /user/terms/accept:
+   *   get:
+   *     tags:
+   *       - user
+   *     description: |
+   *                  ### Accept terms and conditions
+   *     security:
+   *       - Bearer: []
+   *       - OAuth2:
+   *         - openid
+   *         - profile
+   *         - email
+   *         - org.cilogon.userinfo
+   *         - offline_access
+   *     responses:
+   *       200:
+   *         description: returns true if accepted
+   */
   app.get('/terms/accept', async (c) => {
-    const user = c.get('user');
-    if (user) {
-      log.debug('checking accepted terms -User: ' + user?.id);
-      console.log(user);
-      const personId = await getPersonId({configuration, user});      
-      const terms = await agreeTerms({ configuration, personId });
-      console.log(terms);
-      return c.json({accept: true});
-    } else {
-      return c.notFound();
+    try {
+      const user = c.get('user');
+      if (user) {
+        log.debug('accept terms - User: ' + user?.id);
+        const personId = await getPersonId({configuration, user});      
+        await agreeTerms({ configuration, personId });
+        return c.json({accept: true});
+      } else {
+        return c.notFound();
+      }
+    } catch (e) {
+      return c.json({error: e.message});
     }
   });
 
--- src/services/comanage.js
+++ src/services/comanage.js
@@ -1,8 +1,4 @@
-//import {getUser} from "./user.js";
-import {createUserMemberships} from "./userMembership.js";
-import {getGroupMembership} from "../services/cilogon.js"
-import {getLogger} from "../services/logger.js";
-import * as utils from "../services/utils.js";
+import {getLogger} from "./logger.js";
 
 const log = getLogger();
 
@@ -14,22 +10,19 @@ const log = getLogger();
 // https://spaces.at.internet2.edu/display/COmanage/CoTermsAndConditions+API#CoTermsAndConditionsAPI-View(perCO)
 // Getting the accepted terms (for the given user)
 // https://spaces.at.internet2.edu/display/COmanage/CoTAndCAgreement+API#CoTAndCAgreementAPI-View(perCOPerson)
-// After this information is retrieved, the response is the terms and conditions 
-// (request #2) which was updated to include a new Accepted key which is generated based on the response from request #3. 
-// If the given term/condition was accepted in the response in request #3 then it's True, otherwise False.
+// After this information is retrieved, the response is an array of agreements
+// If terms are updated they will be removed from the array. 
+// If the given term/condition was accepted in the past, it will be added to the array.
 // For the POST request to accept the Ts&Cs invoke the following:
 // https://spaces.at.internet2.edu/display/COmanage/CoTAndCAgreement+API#CoTAndCAgreementAPI-Add
 
 export async function getTerms({configuration}) {
   try {
     log.debug('getCoManageTerms');     
-    
     const conf = configuration.api.authentication['identity'];
     const credentials = Buffer.from(`${conf.admin.username}:${conf.admin.apiKey}`).toString('base64');
-    const api = conf.api;
     const terms = `/co_terms_and_conditions/${conf.terms.id}.json`;
     const url = `${conf.admin.host}${terms}`;
-    console.log(url);
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -37,37 +30,35 @@ export async function getTerms({configuration}) {
         'Content-Type': 'json/application;charset=UTF-8'
       }
     });
-    if(!response.ok) {
-      throw new Error(`Error fetching terms for COID ${conf.admin.group} Status: ${response.status} ${response.statusText}`);
-    }else{
+    if(response.ok) {
       const data = await response.json();
       const terms = data?.CoTermsAndConditions;
       if(terms?.length > 0) {
         const data = terms[0];          
-        console.log('Response data:', data);
         return {
           id: data?.Id,
           body: data?.Body,
           url: data?.Url,
           description: data?.Description
         };
+      } else {
+        throw new Error(`Error fetching terms for ID: ${conf.terms.id} Status: ${response.status} ${response.statusText}`);
       }
+    } else {
+      throw new Error(`Error fetching terms ID: ${conf.terms.id} Status: ${response.status} ${response.statusText}`);
     }
   } catch (e) {
-    return {error: e}
+    return {error: e.message};
   }
 }
 
 export async function termsAggrement({configuration, personId}) {
   try {
-    log.debug('getCoManageTerms');
-
+    log.debug('getCoManageTermsAggrement');
     const conf = configuration.api.authentication['identity'];
     const credentials = Buffer.from(`${conf.admin.username}:${conf.admin.apiKey}`).toString('base64');
-    const api = conf.api;
     const termsAgreement = `/co_t_and_c_agreements.json?copersonid=${personId}`;
     const url = `${conf.admin.host}${termsAgreement}`;
-    console.log(url);
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -75,11 +66,8 @@ export async function termsAggrement({configuration, personId}) {
         'Content-Type': 'json/application;charset=UTF-8'
       }
     });
-    console.log('termsAggrement');
-    const data = response;
-    console.log('Response data:', data);
+    if(response.ok) {
     const json = await response.json();
-    console.log('Response data:', json);
     let aggreed = false;
     if(json?.CoTAndCAgreements?.length > 0) {
       for(const agreement of json?.CoTAndCAgreements) {
@@ -89,21 +77,21 @@ export async function termsAggrement({configuration, personId}) {
       }
     } 
     return aggreed;
+  } else { 
+      throw new Error(`Error fetching terms agreement for person: ${personId} Status: ${response.status} ${response.statusText}`);
+    }
   } catch (e) {
-    return {error: e}
+    return {error: e.message}
   }
 }
 
 export async function agreeTerms({configuration, personId}) {
   try {
-    log.debug('getCoTermsAgreement');
-
+    log.debug('postAgreeToTermsAgreement');
     const conf = configuration.api.authentication['identity'];
     const credentials = Buffer.from(`${conf.admin.username}:${conf.admin.apiKey}`).toString('base64');
-    const api = conf.api;
     const termsAgreement = `/co_t_and_c_agreements.json`;
     const url = `${conf.admin.host}${termsAgreement}`;
-    console.log(url);
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -128,13 +116,12 @@ export async function agreeTerms({configuration, personId}) {
     });
     if(!response.ok) {
       throw new Error(`Error accepting termsId ${conf.terms.id} Status: ${response.status} ${response.statusText}`);
-    }else{
+    } else {
       const data = await response.json();
-      console.log('Response data:', data);
       return data;
     }
   } catch (e) {
-    return {error: e}
+    return {error: e.message};
   }
 }
 
@@ -142,14 +129,11 @@ export async function agreeTerms({configuration, personId}) {
 
 export async function getPersonId({configuration, user}) {
   try {
-    log.debug('getCoManageTerms');
-
+    log.debug('getCoManagePersonId');
     const conf = configuration.api.authentication['identity'];
     const credentials = Buffer.from(`${conf.admin.username}:${conf.admin.apiKey}`).toString('base64');
-    const api = conf.api;
     const person = `/co_people.json?coid=${conf.admin.group}&search.identifier=${user.providerId}`;
     const url = `${conf.admin.host}${person}`;
-    console.log(url);
     const response = await fetch(url, {
       method: 'GET',    
       headers: {
@@ -164,13 +148,11 @@ export async function getPersonId({configuration, user}) {
       if(data?.CoPeople?.length > 0) {
         const personId = data?.CoPeople[0]?.Id;
         return personId;
-      }else{
+      } else {
         return null;
       }
     }
-    
-
   } catch (e) {
-    return {error: e}
+    return {error: e.message}
   }
 }
